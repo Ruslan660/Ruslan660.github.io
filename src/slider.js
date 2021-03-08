2e36d136a8ac96830aa71c3dffdf16bfaf10bac7
Ruslan Yestemessov <43512060+Ruslan660@users.noreply.github.com>
2021-03-08 19:27:20 +0600

    slider optimized

--- src/slider.js
+++ src/slider.js
@@ -23,17 +23,12 @@ const entities = [
 ]
 
 let slider_counter = 0;
-
-const menu_point = document.querySelectorAll('.menu-point');
 const l_btn = document.querySelector('.arrow-left');
 const r_btn = document.querySelector('.arrow-right');
-const f_link = document.querySelector('#gallery__f_link');
-const s_link = document.querySelector('#gallery__s_link');
-const t_link = document.querySelector('#gallery__t_link');
 const img = document.querySelector('.gallery__photo');
 const features = document.querySelectorAll('.features__text');
-
-console.log(features);
+const dots = document.querySelector('.slider__dots');
+const links = Array.from(document.querySelectorAll('.gallery__link'));
 
 function setEntity(index) {
     img.style.backgroundImage = `url(${entities[index].img})`;
@@ -43,165 +38,69 @@ function setEntity(index) {
     features[3].innerHTML = entities[index].cost;
 }
 
-f_link.addEventListener('click', () => {
-    if(f_link.classList.contains("active__link")) {
+function slider(index) {
+    setEntity(index);
+    unmakeDotAndLinkActive(slider_counter);
+    makeDotAndLinkActive(index);
+    slider_counter = index;
+}
 
+function addListeners(arr) {
+    arr.forEach((item, id) => {
+        item.addEventListener("click", (e) => {
+            e.preventDefault();
+            slider(id);
+        })
+    })
+}
+
+function initDots() {
+    for(let i = 0;i < entities.length;i++){
+        dots.innerHTML += `<img src="src/img/menu-point.svg" alt="" class="menu-point">`;
+    }
+}
+
+function makeDotAndLinkActive(index) {
+    if (menu_point[index].classList.contains('menu-point--active')){
+
+    }
+    else if(links[index].classList.contains('active__link')) {
+        menu_point[index].classList.add('menu-point--active');
     }
     else {
-        s_link.classList.remove("active__link");
-        menu_point[1].classList.remove("menu-point--active");
-        t_link.classList.remove("active__link");
-        menu_point[2].classList.remove("menu-point--active");
-        f_link.classList.add("active__link");
-        menu_point[0].classList.add("menu-point--active");
-        setEntity(0);
-        slider_counter = 0;
-
+        menu_point[index].classList.add('menu-point--active');
+        links[index].classList.add('active__link');
     }
-})
+}
 
-s_link.addEventListener('click', () => {
-    if(s_link.classList.contains("active__link")) {
+function unmakeDotAndLinkActive(index){
+    menu_point[index].classList.remove('menu-point--active');
+    links[index].classList.remove('active__link');
+}
 
-    }
-    else {
-        f_link.classList.remove("active__link");
-        menu_point[0].classList.remove("menu-point--active");
-        t_link.classList.remove("active__link");
-        menu_point[2].classList.remove("menu-point--active");
-        s_link.classList.add("active__link");
-        menu_point[1].classList.add("menu-point--active");
-        setEntity(1);
-        slider_counter = 1;
-    }
-})
+initDots();
 
-t_link.addEventListener('click', () => {
-    if(t_link.classList.contains("active__link")) {
+const menu_point = Array.from(document.querySelectorAll('.menu-point'));
 
-    }
-    else {
-        f_link.classList.remove("active__link");
-        menu_point[0].classList.remove("menu-point--active");
-        s_link.classList.remove("active__link");
-        menu_point[1].classList.remove("menu-point--active");
-        t_link.classList.add("active__link");
-        menu_point[2].classList.add("menu-point--active");
-        setEntity(2);
-        slider_counter = 2;
-    }
-})
+makeDotAndLinkActive(0);
 
 l_btn.addEventListener('click', () => {
-    if(slider_counter === 0) {
-        f_link.classList.remove("active__link");
-        menu_point[0].classList.remove("menu-point--active");
-        s_link.classList.remove("active__link");
-        menu_point[1].classList.remove("menu-point--active");
-        t_link.classList.add("active__link");
-        menu_point[2].classList.add("menu-point--active");
-        setEntity(2);
-        slider_counter = 2;
-    }
-    else if(slider_counter === 1) {
-        s_link.classList.remove("active__link");
-        menu_point[1].classList.remove("menu-point--active");
-        t_link.classList.remove("active__link");
-        menu_point[2].classList.remove("menu-point--active");
-        f_link.classList.add("active__link");
-        menu_point[0].classList.add("menu-point--active");
-        setEntity(0);
-        slider_counter = 0;
+    if(slider_counter === 0){
+        slider(entities.length - 1);
     }
     else {
-        f_link.classList.remove("active__link");
-        menu_point[0].classList.remove("menu-point--active");
-        t_link.classList.remove("active__link");
-        menu_point[2].classList.remove("menu-point--active");
-        s_link.classList.add("active__link");
-        menu_point[1].classList.add("menu-point--active");
-        setEntity(1);
-        slider_counter = 1;
+        slider(slider_counter - 1);
     }
 })
 
 r_btn.addEventListener('click', () => {
-    if (slider_counter === 2) {
-        s_link.classList.remove("active__link");
-        menu_point[1].classList.remove("menu-point--active");
-        t_link.classList.remove("active__link");
-        menu_point[2].classList.remove("menu-point--active");
-        f_link.classList.add("active__link");
-        menu_point[0].classList.add("menu-point--active");
-        setEntity(0);
-        slider_counter = 0;
-    }
-    else if (slider_counter === 1) {
-        f_link.classList.remove("active__link");
-        menu_point[0].classList.remove("menu-point--active");
-        s_link.classList.remove("active__link");
-        menu_point[1].classList.remove("menu-point--active");
-        t_link.classList.add("active__link");
-        menu_point[2].classList.add("menu-point--active");
-        setEntity(2);
-        slider_counter = 2;
+    if(slider_counter === entities.length - 1){
+        slider(0);
     }
     else {
-        f_link.classList.remove("active__link");
-        menu_point[0].classList.remove("menu-point--active");
-        t_link.classList.remove("active__link");
-        menu_point[2].classList.remove("menu-point--active");
-        s_link.classList.add("active__link");
-        menu_point[1].classList.add("menu-point--active");
-        setEntity(1);
-        slider_counter = 1;
+        slider(slider_counter + 1);
     }
 })
 
-menu_point[0].addEventListener('click', () => {
-    if(f_link.classList.contains("active__link")) {
-
-    }
-    else {
-        s_link.classList.remove("active__link");
-        menu_point[1].classList.remove("menu-point--active");
-        t_link.classList.remove("active__link");
-        menu_point[2].classList.remove("menu-point--active");
-        f_link.classList.add("active__link");
-        menu_point[0].classList.add("menu-point--active");
-        setEntity(0);
-        slider_counter = 0;
-    }
-})
-
-menu_point[1].addEventListener('click', () => {
-    if(s_link.classList.contains("active__link")) {
-
-    }
-    else {
-        f_link.classList.remove("active__link");
-        menu_point[0].classList.remove("menu-point--active");
-        t_link.classList.remove("active__link");
-        menu_point[2].classList.remove("menu-point--active");
-        s_link.classList.add("active__link");
-        menu_point[1].classList.add("menu-point--active");
-        setEntity(1);
-        slider_counter = 1;
-    }
-})
-
-menu_point[2].addEventListener('click', () => {
-    if(t_link.classList.contains("active__link")) {
-
-    }
-    else {
-        f_link.classList.remove("active__link");
-        menu_point[0].classList.remove("menu-point--active");
-        s_link.classList.remove("active__link");
-        menu_point[1].classList.remove("menu-point--active");
-        t_link.classList.add("active__link");
-        menu_point[2].classList.add("menu-point--active");
-        setEntity(2);
-        slider_counter = 2;
-    }
-})
+addListeners(menu_point);
+addListeners(links);
